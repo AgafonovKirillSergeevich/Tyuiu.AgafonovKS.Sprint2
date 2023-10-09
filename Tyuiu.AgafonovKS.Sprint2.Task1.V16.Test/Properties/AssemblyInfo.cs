86059dbc8ca868a7397d2338af6d802ea3c83de4
AgafonovKirill <Kirya_agafonov_2002@mail.ru>
2023-10-09 18:21:06 +0500

    Task1

---
+++ Tyuiu.AgafonovKS.Sprint2.Task1.V16.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.AgafonovKS.Sprint2.Task1.V16.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.AgafonovKS.Sprint2.Task1.V16.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fdbc21b8-c2cb-4404-adc3-f2858d47e2b1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
