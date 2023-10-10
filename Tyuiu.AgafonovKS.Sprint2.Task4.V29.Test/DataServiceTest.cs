249f97e6d3eb683f19ebe61409c9b06c14b28aaa
AgafonovKirill <Kirya_agafonov_2002@mail.ru>
2023-10-10 15:46:11 +0500

    Task4

---
+++ Tyuiu.AgafonovKS.Sprint2.Task4.V29.Test/DataServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.AgafonovKS.Sprint2.Task4.V29.Lib;
+
+namespace Tyuiu.AgafonovKS.Sprint2.Task4.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 3;
+            double y = 23;
+            double res = ds.Calculate(x,y);
+            double wait = 281.333;
+            Assert.AreEqual(wait,res);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 3;
+            double y = 1;
+            double res = ds.Calculate(x, y);
+            double wait = 7;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
